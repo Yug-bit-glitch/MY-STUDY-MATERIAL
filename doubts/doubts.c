93de0f03588f8afcd2157593f9e1b99db1004c92
Yug-bit-glitch <yugpatel2876@gmail.com>
2025-10-25 15:26:36 +0530

    Add pointer manipulation example in doubts.c

--- doubts/doubts.c
+++ doubts/doubts.c
@@ -12,3 +12,20 @@ int main() {
     return 0;
 }
 // when r is derefernced once we do p=q
+
+#include <stdio.h>
+
+int main() {
+    int a = 5, b = 6, c = 7;
+    int *p1 = &a, *p2 = &b;
+    int **q1 = &p1, **q2 = &p2;
+    int ***r = &q1;
+
+    **q2 = **q1 + 2;
+    *r = q2;
+    ***r = ***r + 3;
+
+    printf("%d %d %d\n", a, b, c);
+    return 0;
+}
+
