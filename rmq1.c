27106c68beaa25035b3116e848f28a8c23e514a8
Yug-bit-glitch <yugpatel2876@gmail.com>
2025-10-29 10:00:33 +0530

    Add brute force minimum range query solution
    Implement brute force solution to find minimum in range with safety check for indices.

---
+++ rmq1.c
@@ -0,0 +1,27 @@
+// this is brute force solution 
+// in this for loop is done from l to r
+// just new part is the safety check of l,r
+// limits.h is included for INT_MAX
+#include <stdio.h>
+#include <limits.h>
+
+int main() {
+    int n, q;
+    scanf("%d %d", &n, &q);
+    int A[n];
+    for (int i = 0; i < n; ++i)
+        scanf("%d", &A[i]);
+
+    while (q--) {
+        int l, r;
+        scanf("%d %d", &l, &r);
+        if (l > r) { int t = l; l = r; r = t; }
+
+        int mn = INT_MAX;
+        for (int i = l; i <= r; ++i)
+            if (A[i] < mn) mn = A[i];
+
+        printf("%d\n", mn);
+    }
+    return 0;
+}
